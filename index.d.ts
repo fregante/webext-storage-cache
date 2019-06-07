66a218f3eedc84b329ded9c9fef7d27ca92fdc7b
Connor Love <connor@dotconnor.com>
2019-06-07 03:33:15 -0400

    initial

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare const webextcache: {
+  get<TValue extends unknown = unknown>(key: string): Promise<TValue | undefined>;
+  has(key: string): Promise<boolean>;
+  set<TValue extends unknown = unknown>(key: string, value: TValue, expiration?: number): Promise<void>;
+}
+
+export default webextcache;
