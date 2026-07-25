60fc743c48f9efa3f8a0563cd45e865a2eb99ca8
Federico <me@fregante.com>
2026-07-26 02:02:07 +0800

    Docs

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,8 @@
+import {clear} from './legacy.js';
+
 export {default as CachedValue} from './cached-value.js';
 export {default as CachedFunction} from './cached-function.js';
 
 export const globalCache = {
-	clear: cache.clear,
+	clear,
 };
